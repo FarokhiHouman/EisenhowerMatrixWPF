33706608e3f3ac61a7f9e364287ab950807e0438
Houman Farokhi <farokhihouman@gmail.com>
2024-08-24 12:07:52 +0330

    Update Target framwork

--- EisenhowerMatrix.WPF/DataAccess/EisenhowerMatrixDataModel.Designer.cs
+++ EisenhowerMatrix.WPF/DataAccess/EisenhowerMatrixDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\GitHub\EisenhowerMatrix.WPF\EisenhowerMatrix.WPF\DataAccess\EisenhowerMatrixDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\proj\EisenhowerMatrix\EisenhowerMatrixWPF\EisenhowerMatrix.WPF\DataAccess\EisenhowerMatrixDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
